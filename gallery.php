c5f32dddde2d30ab20b009c781713a637e701f8e
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2019-12-16 12:58:24 -0500

    gallery tab(filter) category works.

--- gallery.php
+++ gallery.php
@@ -6,7 +6,7 @@ if (session_status() == PHP_SESSION_NONE) {
 $db = mysqli_connect('localhost','root','','photography');
 ?>
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <title>Gallery</title>
     <meta charset="utf-8" />
@@ -29,11 +29,11 @@ $db = mysqli_connect('localhost','root','','photography');
             <header class="major">
                 <h1>Gallery</h1>
                 <ul class="actions">
-                    <li><button id="all" type="reset" value="All" >All</button></li>
-                    <li><button id="travel" type="reset" value="Travel" onclick="travelSelect();">Travel</button></li>
-                    <li><button id="events" type="reset" value="Events" onclick="eventsSelect();">Events</button></li>
-                    <li><button id="brands" type="reset" value="Brands" onclick="brandsSelect();">Brands</button></li>
-                    <li><button id="individual" type="reset" value="Individual" onclick="individualSelect();">Individual</button></li>
+                    <li><button id="all" type="reset" value="All" onclick="location.href= './gallery.php?categorySelect=all'" >All</button></li>
+                    <li><button id="travel" type="reset" value="Travel" onclick="location.href= './gallery.php?categorySelect=travel'"  >Travel</button></li>
+                    <li><button id="events" type="reset" value="Events" onclick="location.href= './gallery.php?categorySelect=events'" >Events</button></li>
+                    <li><button id="brands" type="reset" value="Brands" onclick="location.href= './gallery.php?categorySelect=brands'" >Brands</button></li>
+                    <li><button id="individual" type="reset" value="Individual" onclick="location.href= './gallery.php?categorySelect=individuals'" >Individual</button></li>
                 </ul>
             </header>
             <div class="row">
@@ -47,91 +47,87 @@ $db = mysqli_connect('localhost','root','','photography');
                     // x Display picture
                     // x Add cancel button
                     // x After form add image succesfull or unsuccessful do something
-                    //  Filter picture by category
+                    // x Filter picture by category
                     //  people can zoom on pictures
                     //  Admin can delete picture
-
-                    $categorySelected = '';?>
-                     <script language='javascript' type='text/javascript'>
-                          function travelSelect(){
-                              <?php $categorySelected = 'travel';?>
-                          }
-                          function eventsSelect(){
-                              <?php $categorySelected = 'events';?>
-                          }
-                      </script></ul>
-                    <?php
-                    echo $categorySelected;
-//                    if(!($categorySelected === '')){
-//                        // Select query for specific gallery elements
-//                        $gallery_check_query = "SELECT * FROM gallery WHERE galleryCategory='$categorySelected'";
-//                    }else {
-                        // Select query for all gallery elementsec
+                    $categorySelected = '';
+                    if(isset($_GET['categorySelect'])) {
+                        $categorySelected = $_GET['categorySelect'];
+                    }
+                    if(!($categorySelected === ''||$categorySelected === 'all')){
+                        // Select query for specific gallery elements
+                        $gallery_check_query = "SELECT * FROM gallery WHERE galleryCategory='$categorySelected'";
+                    }else {
+//                         Select query for all gallery elements
                         $gallery_check_query = "SELECT * FROM gallery";
-//                        }
+                        }
                     $gallery_result = mysqli_query($db, $gallery_check_query);
 
-                    // Loop through all images
-                    while($gallery = mysqli_fetch_assoc($gallery_result)){
-                        $images[] = $gallery['galleryImage'];
+                    if (mysqli_affected_rows($db) >= 1) {
+                        // Loop through all images
+                        while ($gallery = mysqli_fetch_assoc($gallery_result)) {
+                            $images[] = $gallery['galleryImage'];
+                        }
+
+                        // Initialize column index
+                        $columnIndex = 1;
+
+                        // Display images
+                        echo '<div class="column 1">';
+
+                        foreach ($images as $image) {
+                            if ($columnIndex > 4) {
+                                $columnIndex = 1;
+                            }
+                            if ($columnIndex == 1) {
+                                echo '<img src="' . $image . '">';
+                            }
+                            $columnIndex++;
+                        }
+                        echo '</div>';
+
+                        $columnIndex = 1;
+                        echo '<div class="column">';
+                        foreach ($images as $image) {
+                            if ($columnIndex > 4) {
+                                $columnIndex = 1;
+                            }
+                            if ($columnIndex == 2) {
+                                echo '<img src="' . $image . '">';
+                            }
+                            $columnIndex++;
+                        }
+                        echo '</div>';
+
+                        $columnIndex = 1;
+                        echo '<div class="column">';
+                        foreach ($images as $image) {
+                            if ($columnIndex > 4) {
+                                $columnIndex = 1;
+                            }
+                            if ($columnIndex == 3) {
+                                echo '<img src="' . $image . '">';
+                            }
+                            $columnIndex++;
+                        }
+                        echo '</div>';
+
+                        $columnIndex = 1;
+                        echo '<div class="column">';
+                        foreach ($images as $image) {
+                            if ($columnIndex > 4) {
+                                $columnIndex = 1;
+                            }
+                            if ($columnIndex == 4) {
+                                echo '<img src="' . $image . '">';
+                            }
+                            $columnIndex++;
+                        }
+                        echo '</div>';
                     }
-
-                     // Initialize column index
-                    $columnIndex = 1;
-
-                    // Display images
-                    echo '<div class="column">';
-
-                    foreach($images as $image) {
-                        if($columnIndex > 4){
-                            $columnIndex = 1;
-                        }
-                        if ($columnIndex == 1) {
-                            echo '<img src="'.$image.'">';
-                        }
-                        $columnIndex ++;
+                    else{
+                        echo '<p>The selected category has no pictures.</p>';
                     }
-                    echo '</div>';
-
-                    $columnIndex = 1;
-                    echo '<div class="column">';
-                    foreach($images as $image) {
-                        if($columnIndex > 4){
-                            $columnIndex = 1;
-                        }
-                        if ($columnIndex == 2) {
-                            echo '<img src="'.$image.'">';
-                        }
-                        $columnIndex ++;
-                    }
-                    echo '</div>';
-
-                    $columnIndex = 1;
-                    echo '<div class="column">';
-                    foreach($images as $image) {
-                        if($columnIndex > 4){
-                            $columnIndex = 1;
-                        }
-                        if ($columnIndex == 3) {
-                            echo '<img src="'.$image.'">';
-                        }
-                        $columnIndex ++;
-                    }
-                    echo '</div>';
-
-                    $columnIndex = 1;
-                    echo '<div class="column">';
-                    foreach($images as $image) {
-                        if($columnIndex > 4){
-                            $columnIndex = 1;
-                        }
-                        if ($columnIndex == 4) {
-                            echo '<img src="'.$image.'">';
-                        }
-                        $columnIndex ++;
-                    }
-                    echo '</div>';
-
                     ?>
             </div>
         </div>
