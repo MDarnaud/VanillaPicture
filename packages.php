522cb7dc1f05754c1719bba39c5b1ac515b81458
Megane <38261962+MDarnaud@users.noreply.github.com>
2020-01-17 12:21:25 -0500

    shoppin page layout done
    Signed-off-by: Megane <38261962+MDarnaud@users.noreply.github.com>

--- packages.php
+++ packages.php
@@ -12,81 +12,113 @@
 	<body class="is-preload">
 
     <?php include './navigation/navigation.php' ?>
-		<!-- One -->
-			<div id="main">
-				<div class="wrapper">
-					<div class="inner">
-						<header class="major">
-							<h1><p>YOUR IDEA</p> <p>OUR MAGIC</p></h1>
-							<p>Elit massa nisl elementum lorem ipsum dolor sit consequat</p>
-						</header>
-						<div class="image main">
-							<img src="images/pic01.jpg" alt="" />
-						</div>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque vehicula eros at condimentum. Pellentesque massa nisl, elementum id mi sed, aliquam gravida diam. Donec condimentum eros magna, ut convallis nunc lacinia ac. Sed pellentesque ante nec pharetra facilisis. Nullam sagittis ullamcorper feugiat. Maecenas sollicitudin tincidunt convallis. Suspendisse vel arcu vitae quam pretium interdum. Sed id felis id enim maximus rutrum quis vitae mauris. Nulla facilisi. Integer pharetra mi diam, at viverra neque tristique vitae. Nunc interdum ipsum porta suscipit sagittis. Nam tempor eros non lorem consequat, vel euismod mi hendrerit. Ut suscipit lacus varius risus finibus faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-						<p>Sed auctor, erat nec tristique aliquam, turpis elit iaculis nunc, et suscipit est orci sit amet orci. Nam lacus nunc, suscipit sit amet purus non, imperdiet lacinia lorem. Nunc posuere ligula sit amet massa auctor feugiat. Nam sed odio ac mauris venenatis molestie et ac enim. Etiam rhoncus libero eros, at bibendum elit molestie in. Quisque lacinia, eros nec elementum dictum, est tellus feugiat tellus, quis volutpat quam massa eget nibh. Maecenas elementum euismod ornare. Duis porttitor feugiat mauris in volutpat. Nam ac nisl vulputate, finibus orci lacinia, facilisis lectus. Nulla diam lorem, dignissim vel laoreet imperdiet, bibendum in felis. Duis porttitor accumsan diam, non venenatis est. Nunc et maximus augue, sit amet convallis est. Sed sed magna justo. Sed sagittis consequat metus, eu vulputate felis rutrum eu.</p>
-						<p>Curabitur facilisis luctus cursus. In ultrices ultrices ante, efficitur efficitur velit maximus blandit. Donec finibus ultrices arcu id scelerisque. Pellentesque ante lectus, feugiat ut sodales nec, faucibus sed nisi. In ornare ultricies euismod. Aenean vehicula velit orci, non mattis dui aliquam at. Nullam a ipsum eu orci hendrerit viverra. Curabitur justo odio, tristique nec ligula sed, tristique auctor mi.</p>
 
-					</div>
-				</div>
-			</div>
+    <header class="major">
+        <h1>SHOP</h1>
+    </header>
 
-		<!-- Three -->
-		<div id="three">
-			<div class="wrapper alt special">
-				<header class="major">
-					<h2>PACKAGES</h2>
-					<p>Sed egestas, ante et vulputate volutpat pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum lorem ipsum dolor sit amet consequat hendrerit tortor semper lorem at felis.</p>
-				</header>
-				<div class="wrapper style3">
-					<div class="inner">
-						<div class="profiles">
-							<div class="profile">
-								<div class="image">
-									<img src="images/pic05.jpg" alt="" />
-								</div>
-								<div class="content">
-									<h3>Ipsum nullam</h3>
-									<p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
-									<ul class="actions">
-										<li><a href="#" class="button small">Buy</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="profile">
-								<div class="image">
-									<img src="images/pic06.jpg" alt="" />
-								</div>
-								<div class="content">
-									<h3>Metus gravida</h3>
-									<p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
-									<ul class="actions">
-										<li><a href="#" class="button small">Buy</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="profile">
-								<div class="image">
-									<img src="images/pic07.jpg" alt="" />
-								</div>
-								<div class="content">
-									<h3>Faucibus lorem</h3>
-									<p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
-									<ul class="actions">
-										<li><a href="#" class="button small">Buy</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<footer>
-					<ul class="actions">
-						<li><a href="C:\Users\MegMeg\Documents\Champlain\Semester_5\Final Project\Vanilla Project\Wireframes\dark\gallery.html" class="button">See Photography Shoots</a></li>
-					</ul>
-				</footer>
-			</div>
-		</div>
+    <!-- Gift Cards -->
+    <div id="three" style="padding-bottom: 5%;">
+        <div class="wrapper alt special">
+            <header class="major">
+                <h2>GIFT CARDS</h2>
+                <p>Sed egestas, ante et vulputate volutpat pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum lorem ipsum dolor sit amet consequat hendrerit tortor semper lorem at felis.</p>
+            </header>
+            <div class="wrapper style3">
+                <div class="inner">
+                    <div class="profiles">
+                        <div class="profile">
+                            <div class="image">
+                                <img src="images/pic05.jpg" alt="" />
+                            </div>
+                            <div class="content">
+                                <h3>Ipsum nullam</h3>
+                                <p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
+                                <ul class="actions">
+                                    <li><a href="#" class="button small">Buy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="profile">
+                            <div class="image">
+                                <img src="images/pic06.jpg" alt="" />
+                            </div>
+                            <div class="content">
+                                <h3>Metus gravida</h3>
+                                <p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
+                                <ul class="actions">
+                                    <li><a href="#" class="button small">Buy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="profile">
+                            <div class="image">
+                                <img src="images/pic07.jpg" alt="" />
+                            </div>
+                            <div class="content">
+                                <h3>Faucibus lorem</h3>
+                                <p>Sed egestas, ante et vulputate volutpat pede semper est luctus metus eu augue. Morbi purus amet libero.</p>
+                                <ul class="actions">
+                                    <li><a href="#" class="button small">Buy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Packages -->
+    <div class="wrapper">
+        <div class="inner">
+            <div class="row">
+                <div class="col-2 col-12-medium"></div>
+                <div class="col-8 col-12-medium">
+                    <h2>PACKAGES</h2>
+                    <h3>Package 1</h3>
+                        <p>
+                            <span class="image left"><img src="images/pic04.jpg" style="height: 200px; width: 200px;" alt="" /></span>
+                            Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget.
+                            tempus euismod. Magna et cursus lorem faucibus vestibulum. Blandit adipiscing eu
+                            felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque
+                            praesent tincidunt felis sagittis eget.
+                        </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2 col-12-medium"></div>
+                <div class="col-8 col-12-medium">
+                    <h3>Package 2</h3>
+                        <p>
+                            <span class="image right"><img src="images/pic03.jpg" style="height: 200px; width: 200px;" alt="" /></span>
+                            Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget.
+                            tempus euismod. Magna et cursus lorem faucibus vestibulum. Blandit adipiscing eu
+                            felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque
+                            praesent tincidunt felis sagittis eget. tempus euismod tempus. Vestibulum ante ipsum
+                        </p>
+                </div>
+            </div>
+            <div class="row"  style="padding-bottom: 2%;">
+                <div class="col-2 col-12-medium"></div>
+                <div class="col-8 col-12-medium">
+                    <h3>Package 3</h3>
+                    <p>
+                        <span class="image left"><img src="images/pic04.jpg" style="height: 200px; width: 200px;" alt="" /></span>
+                        Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget.
+                        tempus euismod. Magna et cursus lorem faucibus vestibulum. Blandit adipiscing eu
+                        felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque
+                        praesent tincidunt felis sagittis eget.
+                    </p>
+                </div>
+            </div>
+                    <ul class="customActions">
+                        <li><a href="C:\Users\MegMeg\Documents\Champlain\Semester_5\Final Project\Vanilla Project\Wireframes\dark\gallery.html" class="button">See Photography Shoots</a></li>
+                    </ul>
+    </div>
+    </div>
+
 
 		<!-- Footer -->
 			<div id="footer">
