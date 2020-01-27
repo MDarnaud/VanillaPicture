425a057727426b4190c098f776ba496a83d18c17
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2020-01-27 12:45:53 -0500

    Test2 for commit

--- Website/Home/homepage.php
+++ Website/Home/homepage.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// connect to the database
+// connect to the databases
 $db = mysqli_connect('localhost','root','','photography');
 
 ?>
