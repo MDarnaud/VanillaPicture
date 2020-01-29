7b28c1b3c83a585905a86bd445138edc89c3a062
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2020-01-29 16:27:35 -0500

    Commented SignOutToRegister.php

--- Website/SignOut/SignOutToRegister.php
+++ Website/SignOut/SignOutToRegister.php
@@ -1,11 +1,20 @@
 <?php
-session_start();
+
+// Start session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Verify if the user is sign in
 if (isset($_SESSION['userSignIn'])) {
+        // Unset sessions, set during the sign in
         unset($_SESSION['userSendEmail']);
         unset($_SESSION['userSignIn']);
         unset($_SESSION['userTypeSignIn']);
+        // Go back to the home page when a user sign out
         header('location: ../Registration/register.php');
 }else{
+    // Go back to the home page if a user is not sign in
     header('location: ../Registration/register.php');
 }
 
