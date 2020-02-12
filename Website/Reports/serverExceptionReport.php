e446afed5a0e35cdb5772f79e9aabdd6d9a262c7
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2020-02-12 08:55:57 -0500

    Reports fixed (without error messages).

--- Website/Reports/serverExceptionReport.php
+++ Website/Reports/serverExceptionReport.php
@@ -5,35 +5,33 @@ $errors = "";
 // connect to the database
 $db = mysqli_connect('localhost','root','','photography');
 
+// Initialize variables
+$errors = "";
+$customer = "";
+$announcement = "";
+$shoot = "";
+$payment = "";
+$period = "";
+$year = "";
+$month = "";
+$age = "";
+$location = "";
+$packages = "";
+$paymentDropDown = "";
+
+
+
 // Include server instead of action
 if(isset($_POST['submit'])) {
 // Receive all input values from the form
     $period = mysqli_real_escape_string($db, $_POST['period']);
-    $year = mysqli_real_escape_string($db, $_POST['dropdownYear']);
-    $month = mysqli_real_escape_string($db, $_POST['dropdownMonth']);
-    $customer = mysqli_real_escape_string($db, $_POST['customer']);
-    $announcement = mysqli_real_escape_string($db, $_POST['announcement']);
-    $shoot = mysqli_real_escape_string($db, $_POST['shoot']);
-    $payment = mysqli_real_escape_string($db, $_POST['payment']);
+    if($period === "year"){
+        $year = mysqli_real_escape_string($db, $_POST['dropdownYear']);
+    }elseif ($period === "month"){
+        $month = mysqli_real_escape_string($db, $_POST['dropdownMonth']);
+    }
 
-    // Filters
-        // Customer
-        $periodAge = mysqli_real_escape_string($db, $_POST['age']);
-        $age = '';
-
-            // Shoot
-                //Location
-                $locationShoot = mysqli_real_escape_string($db, $_POST['locationShoot']);
-                $location = '';
-                //Package
-                $packageShoot = mysqli_real_escape_string($db, $_POST['packages']);
-                $packages = '';
-            //Payment
-            $paymentShoot = mysqli_real_escape_string($db, $_POST['paymentAmount']);
-            $paymentDropDown = '';
-
-
-
+    //Errors
     if (empty($period)) {
         $errors = "Select a Year or Month. ";
     }
@@ -41,27 +39,60 @@ if(isset($_POST['submit'])) {
         $errors = "Select a precise period. ";
     }
 
+    if($errors === '') {
+        if ($year != "" || $month != "") {
+            if (isset($_POST['customer'])) {
+                $customer = mysqli_real_escape_string($db, $_POST['customer']);
+                // Filters
+                // Customer
+                $periodAge = mysqli_real_escape_string($db, $_POST['age']);
+                $age = '';
 
-    if($errors === ''){
-        if($periodAge === 'over'){
-            $age = 'over';
-        }
-        elseif ($periodAge === 'under'){
-            $age = 'under';
-        }
+                // Variables
+                if ($periodAge === 'over') {
+                    $age = 'over';
+                } elseif ($periodAge === 'under') {
+                    $age = 'under';
+                }
+            }
+            if (isset($_POST['announcement'])) {
+                $announcement = mysqli_real_escape_string($db, $_POST['announcement']);
+                //Filters
 
-        if($locationShoot != ''){
-            $location = $locationShoot;
-        }
+            }
+            if (isset($_POST['shoot'])) {
+                $shoot = mysqli_real_escape_string($db, $_POST['shoot']);
+                // Filters
+                //Location
+                $locationShoot = mysqli_real_escape_string($db, $_POST['locationShoot']);
+                $location = '';
+                //Package
+                $packageShoot = mysqli_real_escape_string($db, $_POST['packages']);
+                $packages = '';
 
-        if($packageShoot != ''){
-            $packages = $packageShoot;
-        }
+                // Variables
+                if ($locationShoot != '') {
+                    $location = $locationShoot;
+                }
+            }
+            if (isset($_POST['payment'])) {
+                $payment = mysqli_real_escape_string($db, $_POST['payment']);
+                // Filters
+                $paymentShoot = mysqli_real_escape_string($db, $_POST['paymentAmount']);
+                $paymentDropDown = '';
 
-        if($paymentShoot !=''){
-            $paymentDropDown = $paymentShoot;
+                // Variables
+                if ($packageShoot != '') {
+                    $packages = $packageShoot;
+                }
+            }
         }
     }
+
+
+
+
+
 
     header('location: ./reportsException.php?reportSelect=exception&errors='.$errors.'&customer='.$customer.'&announcement='.$announcement
         .'&shoot='.$shoot.'&payment='.$payment.'&period='.$period.'&year='.$year.'&month='.$month.'&age='.$age
