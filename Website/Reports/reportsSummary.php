26cb4bb6313dabbe22fa916d5bfc4766b87585b8
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2020-02-19 14:43:31 -0500

    event sql fixed

--- Website/Reports/reportsSummary.php
+++ Website/Reports/reportsSummary.php
@@ -32,7 +32,7 @@ if(isset($_SESSION['userSignIn'])){
 
                     // % booked hours (available / booked)
                     // MEGANE (count number of available hour where date put = this year, count number of hours shoot booked where date booked = this year, calculate %)
-                    $offeredShoot_summary_y_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE year(start)='$currentYear' AND url IS NOT NULL ";
+                    $offeredShoot_summary_y_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE year(eventStart)='$currentYear' AND eventUrl IS NOT NULL ";
                     $offeredShoot_result = mysqli_query($db, $offeredShoot_summary_y_query);
                     $totalofferedShootY = mysqli_fetch_assoc($offeredShoot_result);
                     $textTotalOfferedShootY = $totalofferedShootY['totalOfferedShoots'];
@@ -359,7 +359,7 @@ for($i=0;$i<5;$i++) {
                     $textNumBookedShootM = $totalBookedShootM['totalShoots'];
 
                     // % booked hours (available / booked)
-                    $offeredShoot_summary_m_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE month(start)='$currentMonth' AND year(start)='$currentYear' AND url IS NOT NULL ";
+                    $offeredShoot_summary_m_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE month(eventStart)='$currentMonth' AND year(eventStart)='$currentYear' AND eventUrl IS NOT NULL ";
                     $offeredShoot_result = mysqli_query($db, $offeredShoot_summary_m_query);
                     $totalofferedShootM = mysqli_fetch_assoc($offeredShoot_result);
                     $textTotalOfferedShootM = $totalofferedShootM['totalOfferedShoots'];
@@ -529,7 +529,7 @@ for($i=0;$i<5;$i++) {
 
                 // % booked hours (available / booked)
                 // MEGANE (count number of available hour where date put = this week, count number of hours shoot booked where date booked = this month, calculate %)
-                    $offeredShoot_summary_w_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE start>='$week_start_day' AND start<='$week_end_day' AND url IS NOT NULL ";
+                    $offeredShoot_summary_w_query ="SELECT count(eventId) as totalOfferedShoots FROM events WHERE eventStart>='$week_start_day' AND eventStart<='$week_end_day' AND eventUrl IS NOT NULL ";
                     $offeredShoot_result = mysqli_query($db, $offeredShoot_summary_w_query);
                     $totalofferedShootW = mysqli_fetch_assoc($offeredShoot_result);
                     $textTotalOfferedShootW = $totalofferedShootW['totalOfferedShoots'];
