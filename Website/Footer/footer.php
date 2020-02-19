4e4d3393b01e0f8731a406e68de9d419c5a32b2a
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2020-02-19 12:18:04 -0500

    email icon

--- Website/Footer/footer.php
+++ Website/Footer/footer.php
@@ -5,7 +5,7 @@
         <ul class="icons footer-icons" >
             <li ><a href=https://www.instagram.com/vanilla_picture/?hl=fr-ca" class="a_socialMedia"><span class="icon fa-instagram"></span><span class="label"></span></a></li>
             <li><a href="http://facebook.com/VANILLAPICTURE/" class="a_socialMedia"><span class="icon fa-facebook"></span><span class="label"></span></a></li>
-            <li><a href="#getInTouch" class="a_socialMedia"><span class="icon fa-envelope"></span><span class="label"></span></a></li>
+            <li><a href="../Home/homepage.php#getInTouch" class="a_socialMedia"><span class="icon fa-envelope"></span><span class="label"></span></a></li>
         </ul>
             Vanilla Picture. &copy; All rights reserved.
         </div>
